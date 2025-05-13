868efb787ad2312c5a8d8fce92d12a6650f3ca39
MUTHUKUMAR K <mail2muthukumar007@gmail.com>
2025-05-13 12:53:45 +0530

    Add files via upload
    Initial commit with the backend files

---
+++ SpringBootRestAPIProject/src/main/java/com/sjprogramming/restapi/SpringBootRestApiProjectApplication.java
@@ -0,0 +1,14 @@
+package com.sjprogramming.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to my page");
+		SpringApplication.run(SpringBootRestApiProjectApplication.class, args);
+	}
+
+}
